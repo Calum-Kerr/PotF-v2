241b363c5665ed4aaef17273273da28facb4d4f8
Calum-Kerr <calumxkerr@gmail.com>
2024-11-16 14:01:06 +0000

    added right movement to the game

--- PotF-v2/Game.cpp
+++ PotF-v2/Game.cpp
@@ -17,6 +17,8 @@ void Game::processEvents() {
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
         mVelocity.x = -0.1f; // adjust this value to control the left movement speed
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        mVelocity.x = 0.1f; // adjust this value to control the right movement speed
     } else {
         mVelocity.x = 0.f;
     }
